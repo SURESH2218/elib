61c9fa9bf982a78d4761467f5853176e1e6125af
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2025-10-09 20:11:32 +0530

    setup express server and nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("welcome to server");
+import app from "./app.js";
+
+const startServer = () => {
+
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+  });
+
+};
+
+startServer();
