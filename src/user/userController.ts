27a8f1414e96a234049cde2ebad1c4747d690cc6
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2025-10-09 22:48:56 +0530

    add user model and user type and checking whether user email exists in the db or not basic validation from db

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,12 +1,21 @@
 import { type Request, type Response, type NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel.js";
 
-const userController = (req: Request, res: Response, next: NextFunction) => {
+const userController = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
 
   //validations
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  //Database call
+  const user = await userModel.findOne({ email });
+
+  if (user) {
+    const error = createHttpError(400, "User already Exists with this email");
     return next(error);
   }
 
---
+++ src/user/userModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { type User } from "./userTypes.js";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model<User>("User", userSchema);
+
+export default userModel;
