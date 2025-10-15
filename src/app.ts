d7dfd2e7c0642e2d0a254e7bcf1bcfb2d7af1467
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2025-10-15 21:11:09 +0530

    testing whether we achieved zero downtime while doing ci

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.get("/health", (req, res) => {
   res.status(200).json({
-    status: "ok",
+    status: "ok testing status for zero down time",
     timestamp: new Date().toISOString(),
     uptime: process.uptime()
   });
